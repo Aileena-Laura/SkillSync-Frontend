40be4b261908bbc850e3911c52d3a29f939261f6
laura ramgil <lauraramgil@hotmail.com>
2024-11-06 12:52:29 +0100

    update API-URL

--- settings.js
+++ settings.js
@@ -1,1 +1,11 @@
-export const API_URL = "http://localhost:8080/api"
+export let API_URL = "";
+if (
+  window.location.hostname === "localhost" ||
+  window.location.hostname === "127.0.0.1"
+) {
+  API_URL = "http://localhost:8080/api";
+} else {
+  //Add URL to your hosted API, once you have it deployed.
+  API_URL =
+    "skillsync-backend-gtgrd5emg5fagudj.northeurope-01.azurewebsites.net";
+}
