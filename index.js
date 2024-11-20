782d6c319d2f9f4d86648f8097cd10737bb8687c
laura ramgil <lauraramgil@hotmail.com>
2024-11-20 13:21:37 +0100

    create strudent and company registration forms with error handling and location API

--- index.js
+++ index.js
@@ -1,73 +1,94 @@
-import "./navigo_EditedByLars.js"  //Will create the global Navigo, with a few changes, object used below
+import "./navigo_EditedByLars.js"; //Will create the global Navigo, with a few changes, object used below
 
 import {
-  setActiveLink, adjustForMissingHash, renderTemplate, loadTemplate
-} from "./utils.js"
+  setActiveLink,
+  adjustForMissingHash,
+  renderTemplate,
+  loadTemplate,
+} from "./utils.js";
 
-import { initLogin,toggleLoginStatus,logout } from "./pages/login/login.js"
-import { initSignup } from "./pages/signup/signup.js"
+import { initLogin, toggleLoginStatus, logout } from "./pages/login/login.js";
+import { initSignupStudent } from "./pages/signup/signup-student.js";
+import { initSignupCompany } from "./pages/signup/signup-company.js";
 
 //If token existed, for example after a refresh, set UI accordingly
-const token = localStorage.getItem("token")
-toggleLoginStatus(token)
-
+const token = localStorage.getItem("token");
+toggleLoginStatus(token);
 
 window.addEventListener("load", async () => {
+  const templateSignupStudent = await loadTemplate(
+    "./pages/signup/signup-student.html"
+  );
+  const templateSignupCompany = await loadTemplate(
+    "./pages/signup/signup-company.html"
+  );
+  const templateLogin = await loadTemplate("./pages/login/login.html");
+  const templateNotFound = await loadTemplate("./pages/notFound/notFound.html");
 
-  const templateSignup = await loadTemplate("./pages/signup/signup.html")
-  const templateLogin = await loadTemplate("./pages/login/login.html")
-  const templateNotFound = await loadTemplate("./pages/notFound/notFound.html")
-
-  adjustForMissingHash()
+  adjustForMissingHash();
 
   const router = new Navigo("/", { hash: true });
   //Not especially nice, BUT MEANT to simplify things. Make the router global so it can be accessed from all js-files
-  window.router = router
+  window.router = router;
 
   router
     .hooks({
       before(done, match) {
-        setActiveLink("menu", match.url)
-        done()
-      }
+        setActiveLink("menu", match.url);
+        done();
+      },
     })
     .on({
       //For very simple "templates", you can just insert your HTML directly like below
-      "/": () => document.getElementById("content").innerHTML = `
+      "/": () =>
+        (document.getElementById("content").innerHTML = `
         <h2>Home</h2>
         <h5 style="color:darkorange">Make sure to change, colors and layout if you use this for your own projects</h5>
-     `,
+     `),
       "/dropdown-0": () => {
-        alert(0)  
+        alert(0);
       },
       "/dropdown-1": () => {
-        alert(1)
+        alert(1);
       },
       "/dropdown-2": () => {
-        alert(2)
+        alert(2);
       },
-      
-      "/signup": () => {
-        renderTemplate(templateSignup, "content")
-        initSignup()
+
+      "/signup/student": () => {
+        renderTemplate(templateSignupStudent, "content");
+        initSignupStudent();
+      },
+      "/signup/company": () => {
+        renderTemplate(templateSignupCompany, "content");
+        initSignupCompany();
       },
       "/login": (match) => {
-        renderTemplate(templateLogin, "content")
-        initLogin(match)
+        renderTemplate(templateLogin, "content");
+        initLogin(match);
       },
       "/logout": () => {
-        ()=> router.navigate("/")
-        logout()
-      }
+        () => router.navigate("/");
+        logout();
+      },
     })
     .notFound(() => {
-      renderTemplate(templateNotFound, "content")
+      renderTemplate(templateNotFound, "content");
     })
-    .resolve()
+    .resolve();
 });
 
-
 window.onerror = function (errorMsg, url, lineNumber, column, errorObj) {
-  alert('Error: ' + errorMsg + ' Script: ' + url + ' Line: ' + lineNumber
-    + ' Column: ' + column + ' StackTrace: ' + errorObj);
-}
+  alert(
+    "Error: " +
+      errorMsg +
+      " Script: " +
+      url +
+      " Line: " +
+      lineNumber +
+      " Column: " +
+      column +
+      " StackTrace: " +
+      errorObj
+  );
+};
--- pages/signup/signup-student.js
+++ pages/signup/signup-student.js
@@ -1,35 +1,34 @@
 import { API_URL } from "../../settings.js";
 import { handleHttpErrors } from "../../utils.js";
 
-const URL = API_URL + "/user-with-role";
+const URL = API_URL + "/user-with-role/student";
 let role = null;
 let responseStatus;
 
-export function initSignup() {
+export function initSignupStudent() {
   responseStatus = document.getElementById("status");
-  document.getElementById("btn-signup").onclick = signup;
+  document.getElementById("btn-signup").onclick = signupStudent;
 }
 
-async function signup(evt) {
+async function signupStudent(evt) {
   evt.preventDefault();
   const username = document.getElementById("input-username").value;
   const email = document.getElementById("input-email").value;
   const password = document.getElementById("input-password").value;
-  const studentChecked = document.getElementById("checkbox-student").checked;
-  const companyChecked = document.getElementById("checkbox-company").checked;
+  const firstName = document.getElementById("input-firstName").value;
+  const lastName = document.getElementById("input-lastName").value;
+  const confirmPassword = document.getElementById(
+    "input-password-confirm"
+  ).value;
 
-  if (!username || !email || !password) {
-    responseStatus.innerText = "All fields are required.";
-    return; // Prevent form submission if any field is empty
+  if (password != confirmPassword) {
+    responseStatus.innerText = "Passwords does not match.";
+    return;
   }
 
-  if (studentChecked) {
-    role = "student";
-  } else if (companyChecked) {
-    role = "company";
-  }
+  role = "STUDENT";
 
-  const user = { username, email, password, role };
+  const user = { username, email, password, role, firstName, lastName };
   const options = {
     method: "POST",
     headers: { "Content-type": "application/json" },
@@ -43,8 +42,11 @@ async function signup(evt) {
   } catch (err) {
     responseStatus.style.color = "darkred";
     if (err.apiError) {
-      responseStatus.innerText =
-        "Registration failed: " + err.apiError.message || "Unknown error";
+      const errorMessages = Object.values(err.apiError);
+      const lastErrorMessage = errorMessages[errorMessages.length - 1];
+
+      // Display only the last error message
+      responseStatus.innerText = "Registration failed: " + lastErrorMessage;
     } else {
       responseStatus.innerText = err.message;
     }
